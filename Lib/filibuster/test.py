471cb209186db9e6421c5ef9dfca61a4286b1215
Erik <erik@letterror.com>
2016-12-21 11:44:49 +0100

    Initial public commit of filibuster.

---
+++ Lib/filibuster/test.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+
+from filibuster.blurb import Blurb
+
+import codecs
+import os
+
+# see if we can generate all of them
+w = Blurb()
+names = w.getBlurbTypes()
+
+dst = os.path.join(os.getcwd(), "test")
+if not os.path.exists(dst):
+	os.makedirs(dst)
+
+maxTests = 10
+for name in names:
+	print "trying",name
+	namePath = os.path.join(dst, "%s.txt"%name)
+	nameTagPath = os.path.join(dst, "%s_trying.txt"%name)
+	if os.path.exists(namePath):
+		print "name with dupe file %s, skipping"%(name)
+		continue
+	t = codecs.open(nameTagPath, 'w', 'utf-8')
+	t.write("a")
+
+	success = False
+	for i in range(maxTests):
+		f = codecs.open(namePath, 'a', 'utf-8')
+		result = w.getBlurb(name)
+		print name, result
+		try:
+			f.write(u"\n\n"+result)
+			success = True
+		except:
+			print "UnicodeDecodeError"
+		finally:
+			f.close()
+
+	if success:
+		t.close()
+		os.remove(nameTagPath)
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = "Filibuster",
+      version = "2",
+      description = "Python generator for nonsense language on all sorts of subjects.",
+      author = "Erik van Blokland, Jonathan Hoefler, Rich Roat, Petr van Blokland, Just van Rossum and many others.",
+      author_email = "erik@letterror.com",
+      url = "https://github.com/LettError/filibuster",
+      license = "MIT",
+      packages = [
+              "filibuster",
+      ],
+      package_dir = {"":"Lib"},
+)
