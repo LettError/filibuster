4d96d0c67696f0f4b953e4d4308044e24907e1b6
Erik van Blokland <erik@letterror.com>
2020-07-07 14:59:40 +0200

    fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "Filibuster",
-      version = "2",
+      version = "3",
       description = "Python generator for nonsense language on all sorts of subjects.",
       author = "Erik van Blokland, Jonathan Hoefler, Rich Roat, Petr van Blokland, Just van Rossum and many others.",
       author_email = "erik@letterror.com",
